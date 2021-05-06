a6830d32366f660957f84bc302633065a4ffb298
Abraao Matias <abraao_matias200@hotmail.com>
2021-05-06 17:38:30 -0300

    Projeto Pagamento de comissao em andamento

---
+++ PagamentoComissao/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+namespace PagamentoComissao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantidade de vendedores cadastrados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Vendedor ven = new Vendedor();
+
+            List<Vendedor> vendedor = new List<Vendedor>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Vendedor #" + i + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Codigo da peca: ");
+                int codigo = int.Parse(Console.ReadLine());
+                Console.Write("Preco unitario: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantidade de peca vendidas: ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                vendedor.Add(new Vendedor(nome, codigo, preco, quantidade));
+                Console.WriteLine();
+            }
+            
+            Console.Write("Deseja fazer alteracao na quantidade? Digite o id que vc quer fazer alteracao: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            ven = vendedor.Find(x => x.CodigoPeca == numero);
+
+            if(ven != null)
+            {
+                Console.Write("Digite o novo valor para a quantidade: ");
+                int quantidade = int.Parse(Console.ReadLine());
+                ven.Quantidade = quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Esse Id da peca nao existe");
+            }
+
+
+            Console.Write("Deseja remover algum vendedor ? Digite o Id dele: ");
+            numero = int.Parse(Console.ReadLine());
+            
+            for(int i = vendedor.Count - 1; i >= 0; i--)
+            {
+                if(vendedor[i].CodigoPeca == numero)
+                {
+                    vendedor.RemoveAt(i);
+                    Console.WriteLine("Usuario excluido com sucesso!");
+                }
+            }
+
+
+            Console.WriteLine();
+            foreach(Vendedor obj in vendedor)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+    }
+}
---
+++ PagamentoComissao/Vendedor.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+
+
+namespace PagamentoComissao
+{
+    class Vendedor
+    {
+
+        public string Nome { get; set; }
+        public int CodigoPeca { get; set; }
+        public double PrecoUnitario { get; set; }
+        public int Quantidade { get; set; }
+
+
+        public Vendedor() { }
+
+        public Vendedor(string nome, int codigoPeca, double precoUnitario, int quantidade)
+        {
+            Nome = nome;
+            CodigoPeca = codigoPeca;
+            PrecoUnitario = precoUnitario;
+            Quantidade = quantidade;
+        }
+
+
+        public double totalVenda
+        {
+            get { return PrecoUnitario * Quantidade; }
+        }
+
+        public double comissao
+        {
+            get { return PrecoUnitario * Quantidade * 0.05;  }
+        }
+
+
+        public override string ToString()
+        {
+            return "Nome: "
+                + Nome
+                + "\n"
+                + "Codigo da Peça: "
+                + CodigoPeca
+                + "\n"
+                + "Preço unitario da peça: "
+                + PrecoUnitario.ToString("F2", CultureInfo.InvariantCulture)
+                + "\n"
+                + "Quatidade vendida: "
+                + Quantidade
+                + "\n"
+                + "Valor total das vendas: "
+                + totalVenda.ToString("F2", CultureInfo.InvariantCulture)
+                + "\n"
+                + "Comissão do vendedor: "
+                + comissao.ToString("F2", CultureInfo.InvariantCulture)
+                + "\n"
+                + "---------------------------------"
+                + "\n";
+               
+        }
+
+
+    }
+}
